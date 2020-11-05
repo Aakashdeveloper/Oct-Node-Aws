f4cde43f60c3011a72eedd930db26bb5c94e3c0a
Aakashdeveloper <ahanda205@gmail.com>
2020-11-05 09:59:35 +0530

    a

--- node/fullstack/src/routes/HotelRouter.js
+++ node/fullstack/src/routes/HotelRouter.js
@@ -16,8 +16,8 @@ function router(menu){
               if(err){
                 res.status(501).send('Error while fetching')
               }else{
-                res.send(data)
-                //res.render('hotel',{title:'Hotels Page',data:data,menu})
+                //res.send(data)
+                res.render('hotel',{title:'Hotels Page',data:data,menu})
               }
             })
           }
@@ -25,9 +25,25 @@ function router(menu){
        
   })
 
-  hotelRouter.route('/details')
+  hotelRouter.route('/details/:id')
     .get(function(req,res){
-        res.send('Hotel Details')
+        //var id = req.params.id;
+        var {id} = req.params
+        mongodb.connect(url,function(err,connection){
+          if(err){
+            res.status(501).send('Error while connecting')
+          }else{
+            const dbo = connection.db('Sjtech')
+            dbo.collection('hotels').findOne({_id:id},function(err,data){
+              if(err){
+                res.status(501).send('Error while fetching')
+              }else{
+                //res.send(data)
+                res.render('hotelDetails',{title:'Hotels Page',data:data,menu})
+              }
+            })
+          }
+        })
   })
 
   return hotelRouter
