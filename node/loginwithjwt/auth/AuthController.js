bb827a7f62d15e571c3495188685654da9b99110
Aakashdeveloper <ahanda205@gmail.com>
2020-11-10 09:53:21 +0530

    a

--- node/loginwithjwt/auth/AuthController.js
+++ node/loginwithjwt/auth/AuthController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const bcrypt = request('bcryptjs')
+const bcrypt = require('bcryptjs')
 const config = require('../config');
 const User = require('./UserSchema');
 
@@ -23,6 +23,31 @@ router.post('/register',(req,res) => {
     })
 })
 
+//loginUser
+router.post('/login',(req,res) => {
+    User.findOne({email:req.body.email},(err,user) => {
+        if(err) return res.status(500).send('Error while login');
+        if(!user) return res.status(400).send('No User Found Register First');
+        else{
+            const passIsValid = bcrypt.compareSync(req.body.password,user.password);
+            if(!passIsValid) return res.status(401).send('Invalid Password');
+            var token = jwt.sign({id:user._id},config.secert,{expiresIn:86400})
+            res.send({auth:true,token:token})
+        }
+    })
+});
+
+//UserInfo
+router.get('/userinfo',(req,res) => {
+    var token = req.headers['x-access-token'];
+    if(!token) res.send({auth:false,token:'No Token Provided'})
+    jwt.verify(token,config.secert,(err,data)=>{
+        if(err) return res.status(500).send({auth:false,token:'Invalid Token Provided'});
+        User.findById(data.id,{password:0},(err,user)=>{
+            res.send(user)
+        })
+    })
+})
 
 ///get All user list
 router.get('/users',(req,res)=>{
